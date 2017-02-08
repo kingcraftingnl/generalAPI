87763f27cd4a653d52213c37c48a5d9940752904
kingcraftingnl <jaspersmit2801@gmail.com>
2017-02-08 20:00:06 +0100

    Added readStringFromFile too StringTools

--- src/main/java/nl/kingcrafting/generalAPI/tools/string/StringTools.java
+++ src/main/java/nl/kingcrafting/generalAPI/tools/string/StringTools.java
@@ -1,9 +1,23 @@
 package nl.kingcrafting.generalAPI.tools.string;
 
+import java.io.File;
+import java.io.IOException;
+
+import nl.kingcrafting.generalAPI.tools.io.FileTools;
+
 public class StringTools {
 
 	public StringTools() {
 		// TODO Auto-generated constructor stub
 	}
+	
+	
+	
+	
+	public static String readStringFromFile(File file) throws IOException{
+		return FileTools.readStringFromFile(file);
+		
+	}
+	
 
 }
